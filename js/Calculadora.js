135f7a004286e6432500d57a4c1f56c51d668d09
soandrade6 <sofia.andradep06@gmail.com>
2024-04-06 09:58:28 -0500

    Calculadora modificada en su totalidad

---
+++ js/Calculadora.js
@@ -0,0 +1,18 @@
+class Calculadora {
+
+    sumar(n1, n2){
+        return n1 + n2;
+    }
+
+    restar(n1, n2){
+        return n1 - n2;
+    }
+
+    dividir(n1, n2){
+        return n1 / n2;
+    }
+
+    multiplicar(n1, n2){
+        return n1 * n2;
+    }
+}
